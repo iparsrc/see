d9afc7990365bd1e1eaee38bf817f250e51d6e59
Parsa Akbari <iparsrc@gmail.com>
2025-12-08 20:05:33 +0100

    wip

--- backend/src/test/java/lu/uni/e4l/platform/ContextLoadsTest.java
+++ backend/src/test/java/lu/uni/e4l/platform/ContextLoadsTest.java
@@ -9,4 +9,15 @@ public class ContextLoadsTest {
     @Test
     public void contextLoads() { }
 
+    @Test
+    public void dummyPrintTest() {
+        System.out.println("Hello, this is a dummy passing test!");
+    }
+
+    @Test
+    public void dummyFailingTest() {
+        System.out.println("Hello, this is a dummy failing test!");
+        org.junit.Assert.fail("Hello, this is a dummy failing test!");
+    }
+
 }
